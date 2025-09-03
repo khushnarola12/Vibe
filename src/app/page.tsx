a5b252849d4c02976ff08c674c72ea8d91b3308e
khushnarola12 <khushnarola12@gmail.com>
2025-09-03 16:16:59 +0530

    05 : ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@
 import { Button } from "@/components/ui/button"
 import { useTRPC } from "@/trpc/client"
 import { useMutation } from "@tanstack/react-query"
+import {Input} from "@/components/ui/input"
 import { toast } from "sonner"
+import { useState } from "react"
 
 const Page = () => {
+
+  const [value,setValue] = useState("")
+
   const trpc = useTRPC()
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess:()=>{
@@ -15,7 +20,8 @@ const Page = () => {
 
   return (
    <div>
-      <Button disabled={invoke.isPending} onClick={()=> invoke.mutate({text:"khush"})}>
+      <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
+      <Button disabled={invoke.isPending} onClick={()=> invoke.mutate({value:value})}>
         Revoke Background Jobs
       </Button>
    </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,29 @@
+import { createAgent ,gemini} from "@inngest/agent-kit";
 import { inngest } from "./client";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    await step.sleep("wait-a-moment", "10s");
+  async ({ event}) => {
+   // Create a Gemini model provider with your API key
+     const codeAgent = createAgent({
+      name: "summarizer",
+      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple next.js & react.js snippets ",
+      model: gemini({
+        model: "gemini-1.5-flash",       // replace with preferred Gemini model
+        apiKey: process.env.GEMINI_API_KEY, // AgentKit will pick this up automatically if set
+        // You can also set defaultParameters here, e.g.:
+        // defaultParameters: { temperature: 0.7 }
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+  `Write the following snippet : ${event.data.value}`,
+);
+console.log(output);
+
     
-    return { message: `Hello ${event.data.email}!` };
+    return { output };
   },
 );
