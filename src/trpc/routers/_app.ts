a21d2cb9f23ce8fc93c52eb839e113393ed50b80
khushnarola12 <khushnarola12@gmail.com>
2025-09-09 10:44:07 +0530

    09 : projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
 import { messagesRouter } from '@/modules/messages/server/procedures';
+import { projectsRouter } from '@/modules/projects/server/procedures';
 export const appRouter = createTRPCRouter({
 
   messages : messagesRouter,
+  projects : projectsRouter,
   
 
 });
