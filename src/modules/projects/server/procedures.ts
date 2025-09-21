eecee0f62494b45c7443e93dfad302d829a5ee9b
khushnarola12 <khushnarola12@gmail.com>
2025-09-21 22:16:24 +0530

    16:authentication

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -1,19 +1,20 @@
 import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/db";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { generateSlug } from "random-word-slugs";
 import { z } from "zod";
 
 export const projectsRouter = createTRPCRouter({
-     getOne : baseProcedure
+     getOne : protectedProcedure
       .input(z.object({
         id: z.string().min(1,{message:"Project ID is required"} )
       }))
-      .query(async({input})=>{
+      .query(async({input,ctx})=>{
         const existingProjects = await prisma.project.findUnique({
             where:{
-                id: input.id
+                id: input.id,
+                userId:ctx.auth.userId
             }
             
         })
@@ -22,26 +23,30 @@ export const projectsRouter = createTRPCRouter({
         }
         return existingProjects
       }),
-    getMany : baseProcedure
+    getMany : protectedProcedure
     
-      .query(async()=>{
+      .query(async({ctx})=>{
         const projects = await prisma.project.findMany({
+            where:{
+                userId:ctx.auth.userId
+            },
             orderBy:{
-                createdAt:"desc"
+                updateAt:"desc"
             },
         })
         return projects
       }),
-    create:baseProcedure
+    create:protectedProcedure
     .input(
         z.object({
             value :z.string().min(1,{message:"Message cannot be empty"}).max(10000,{message:"Message is too long"})
         }),
     )
-    .mutation(async({input})=>{
+    .mutation(async({input,ctx})=>{
 
         const createdProject = await prisma.project.create({
             data:{
+                userId:ctx.auth.userId,
                 name: generateSlug(2,{
                     format : 'kebab'
                 }),
